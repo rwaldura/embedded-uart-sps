38923e5be35293bd67dd844b5c95ed4feede012b
Ren Waldura <ren@waldura.org>
2020-12-16 21:20:46 -0800

    setenv DEBUG=1 to trigger debugging messages

--- sps30-uart/sps30_example_usage.c
+++ sps30-uart/sps30_example_usage.c
@@ -32,6 +32,7 @@
 #include <stdio.h>  // printf
 #include <math.h>   // roundf()
 #include <time.h>   // time()
+#include <stdlib.h> // getenv()
 
 #include "sensirion_uart.h"
 #include "sps30.h"
@@ -91,8 +92,9 @@ struct sps30_measurement average_measurements(const struct sps30_measurement mm[
 int main(int argc, const char* argv[]) {
     char serial[SPS30_MAX_SERIAL_LEN];
     const uint8_t AUTO_CLEAN_DAYS = 4;
-    const int NUM_SAMPLES = 60;
     int16_t ret;
+    const int NUM_SAMPLES = 60;
+    const int DEBUG = getenv("DEBUG") != NULL;
 
     while (sensirion_uart_open() != 0) {
         fprintf(stderr, "UART init failed\n");
@@ -106,14 +108,15 @@ int main(int argc, const char* argv[]) {
         fprintf(stderr, "SPS30 sensor probing failed\n");
         sensirion_sleep_usec(1000000); /* sleep for 1s */
     }
-    fprintf(stderr, "SPS30 sensor probing successful\n");
+    if (DEBUG) fprintf(stderr, "SPS30 sensor probing successful\n");
 
     struct sps30_version_information version_information;
     ret = sps30_read_version(&version_information);
     if (ret) {
         fprintf(stderr, "error %d reading version information\n", ret);
     } else {
-        fprintf(stderr, "FW: %u.%u HW: %u, SHDLC: %u.%u\n",
+        if (DEBUG)
+            fprintf(stderr, "FW: %u.%u HW: %u, SHDLC: %u.%u\n",
                version_information.firmware_major,
                version_information.firmware_minor,
                version_information.hardware_revision,
@@ -125,7 +128,7 @@ int main(int argc, const char* argv[]) {
     if (ret)
         fprintf(stderr, "error %d reading serial\n", ret);
     else
-        fprintf(stderr, "SPS30 Serial: %s\n", serial);
+        if (DEBUG) fprintf(stderr, "SPS30 Serial: %s\n", serial);
 
     ret = sps30_set_fan_auto_cleaning_interval_days(AUTO_CLEAN_DAYS);
     if (ret)
@@ -139,22 +142,25 @@ int main(int argc, const char* argv[]) {
     while (1) {
         ret = sps30_start_measurement();
         if (ret < 0) {
+            fprintf(stderr, "error starting measurement\n");
             sensirion_sleep_usec(1000000); /* sleep for 1s */
             break;
         }
 
-        fprintf(stderr, "measurements started\n");
-        fprintf(stderr, "#"
-                       "\tpm1.0"
-                       "\tpm2.5"
-                       "\tpm4.0"
-                       "\tpm10.0"
-                       "\tnc0.5"
-                       "\tnc1.0"
-                       "\tnc2.5"
-                       "\tnc4.5"
-                       "\tnc10.0"
-                       "\ttps\n");
+        if (DEBUG) {
+	        fprintf(stderr, "measurements started\n");
+	        fprintf(stderr, "#"
+	                       "\tpm1.0"
+	                       "\tpm2.5"
+	                       "\tpm4.0"
+	                       "\tpm10.0"
+	                       "\tnc0.5"
+	                       "\tnc1.0"
+	                       "\tnc2.5"
+	                       "\tnc4.5"
+	                       "\tnc10.0"
+	                       "\ttps\n");        	
+        }
 
         // collect a batch of measurements, then average them out
         struct sps30_measurement batch[NUM_SAMPLES];
@@ -175,43 +181,44 @@ int main(int argc, const char* argv[]) {
                     SPS30_GET_ERR_STATE(ret), i);
             } else {
                 batch[i] = m;
-                fprintf(stderr, "%d"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f"
-                       "\t%0.2f\n",
-                       i, m.mc_1p0, m.mc_2p5, m.mc_4p0, m.mc_10p0, m.nc_0p5,
-                       m.nc_1p0, m.nc_2p5, m.nc_4p0, m.nc_10p0,
-                       m.typical_particle_size);
+                if (DEBUG)
+                    fprintf(stderr, "%d"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f"
+                        "\t%0.2f\n",
+                        i, m.mc_1p0, m.mc_2p5, m.mc_4p0, m.mc_10p0, m.nc_0p5,
+                        m.nc_1p0, m.nc_2p5, m.nc_4p0, m.nc_10p0,
+                        m.typical_particle_size);
             }
         }
 
         struct sps30_measurement m = average_measurements(batch, NUM_SAMPLES);
         if (m.typical_particle_size > 0) // valid measurement
             printf("%ld" 
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d"
-               "\t%d\n",
-               time(NULL),
-               /* Round all measured values; fractional digits do not carry any valid information.
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d"
+                "\t%d\n",
+                time(NULL),
+                /* Round all measured values; fractional digits do not carry any valid information.
                 * See https://github.com/Sensirion/embedded-uart-sps/issues/77
                 */
-               _round(m.mc_1p0), _round(m.mc_2p5), _round(m.mc_4p0), _round(m.mc_10p0), _round(m.nc_0p5),
-               _round(m.nc_1p0), _round(m.nc_2p5), _round(m.nc_4p0), _round(m.nc_10p0),
-               _roundK(m.typical_particle_size));
+                _round(m.mc_1p0), _round(m.mc_2p5), _round(m.mc_4p0), _round(m.mc_10p0), _round(m.nc_0p5),
+                _round(m.nc_1p0), _round(m.nc_2p5), _round(m.nc_4p0), _round(m.nc_10p0),
+                _roundK(m.typical_particle_size));
 
         /* Stop measurement for 1min to preserve power. Also enter sleep mode
          * if the firmware version is >=2.0.
@@ -228,7 +235,7 @@ int main(int argc, const char* argv[]) {
             }
         }
 
-        fprintf(stderr, "No measurements for 1 minute\n");
+        if (DEBUG) fprintf(stderr, "No measurements for 1 minute\n");
         sensirion_sleep_usec(1000000 * 60);
 
         if (version_information.firmware_major >= 2) {
